e303a408f78923ea490143766f302ebc401938ce
gatsby6060 <uniline123@naver.com>
2025-10-22 12:37:59 +0900

    first commit

---
+++ teamprojectjgh/src/test/java/com/example/teamprojectjgh/TeamprojectjghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamprojectjgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectjghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
